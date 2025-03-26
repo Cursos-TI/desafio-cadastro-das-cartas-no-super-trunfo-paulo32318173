694b8aa7a444778cc16e95e81bb0b53fbefa2ee0
PauloCB <paulocesarbonelli@hotmail.com>
2025-03-26 22:57:35 +0000

    commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,6 +8,7 @@
 
 int main() {
     printf("Cartas super trunfo! \n");
+    printf("Novo Commit \n");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
